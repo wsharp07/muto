fd08f0d97de7632b96d6ae7a7e9c8e3040bae573
William Sharp <wsharp07@gmail.com>
2023-07-19 21:44:46 -0400

    fixing spacing

--- src/commands/migrate/create.ts
+++ src/commands/migrate/create.ts
@@ -6,18 +6,18 @@ export default class MigrateCreate extends Command {
 
 	static examples = ['<example>'];
 
-  static flags = {
-    before: Flags.boolean({
-      char: 'b',
-      description: 'Create a Before script',
-      required: false,
-    }),
-    after: Flags.boolean({
-      char: 'a',
-      description: 'Create a After script',
-      required: false,
-    }),
-  };
+	static flags = {
+		before: Flags.boolean({
+			char: 'b',
+			description: 'Create a Before script',
+			required: false,
+		}),
+		after: Flags.boolean({
+			char: 'a',
+			description: 'Create a After script',
+			required: false,
+		}),
+	};
 
 	static args = {
 		name: Args.string({ description: 'Name of the migration', required: true }),
--- src/core/migrate.ts
+++ src/core/migrate.ts
@@ -19,18 +19,22 @@ const createFile = (dir: string, name: string, content: string): void => {
 };
 
 const createMigration = (
-  name: string,
-  options: { before: boolean; after: boolean }
+	name: string,
+	options: { before: boolean; after: boolean },
 ): void => {
-  createBaseDirectory();
-  const dir = `${DEFAULT_MIGRATION_DIR}/${Date.now()}-${name}`;
-  createDirectory(dir);
-  createFile(dir, 'up.sql', '## Write your up migration here\n');
-  createFile(dir, 'down.sql', '## Write your down migration here\n');
-  options.before &&
-    createFile(dir, 'before.sql', '## Write your before migration here\n');
-  options.after &&
-    createFile(dir, 'after.sql', '## Write your after migration here\n');
+	createBaseDirectory();
+	const dir = `${DEFAULT_MIGRATION_DIR}/${Date.now()}-${name}`;
+	createDirectory(dir);
+	createFile(dir, 'up.sql', '## Write your up migration here\n');
+	createFile(dir, 'down.sql', '## Write your down migration here\n');
+
+	if (options.before) {
+		createFile(dir, 'before.sql', '## Write your before migration here\n');
+	}
+
+	if (options.after) {
+		createFile(dir, 'after.sql', '## Write your after migration here\n');
+	}
 };
 
 const runMigration = async (): Promise<void> => {
