cd7e9e7b3749e5e9ac70b45937d842dbecf2f500
William Sharp <wsharp07@gmail.com>
2023-07-21 00:10:40 -0400

    so many fixes

--- src/core/database/postgres.ts
+++ src/core/database/postgres.ts
@@ -1,39 +1,39 @@
-import { type IDatabaseAdapter } from '../interface';
 import createPostgresConnectionPool, {
-	type ConnectionPool,
-	sql,
+  type ConnectionPool,
+  sql,
 } from '@databases/pg';
+import { type IDatabaseAdapter } from '../interface';
 
 export class PostgresDatabaseAdapter implements IDatabaseAdapter {
-	private readonly db: ConnectionPool;
-	constructor(private readonly connectionString: string) {
-		this.db = createPostgresConnectionPool({
-			bigIntMode: 'bigint',
-			connectionString: this.connectionString,
-		});
-	}
+  private readonly db: ConnectionPool;
+  constructor(private readonly connectionString: string) {
+    this.db = createPostgresConnectionPool({
+      bigIntMode: 'bigint',
+      connectionString: this.connectionString,
+    });
+  }
 
-	async dispose(): Promise<void> {
-		await this.db.dispose();
-	}
+  async dispose(): Promise<void> {
+    await this.db.dispose();
+  }
 
-	async createMigrationTable(): Promise<void> {
-		await this.db.query(sql`
-			CREATE TABLE IF NOT EXISTS migrations (
-			  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-			  name TEXT NOT NULL,
-			  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
-			);
-		`);
-	}
+  async createMigrationTable(): Promise<void> {
+    await this.db.query(sql`
+      CREATE TABLE IF NOT EXISTS migrations (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        name TEXT NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+      );
+    `);
+  }
 
-	async getLatestMigration(): Promise<void> {
-		await this.db.query(sql`
-			SELECT name FROM migrations ORDER BY id DESC LIMIT 1;
-		`);
-	}
+  async getLatestMigration(): Promise<void> {
+    await this.db.query(sql`
+      SELECT name FROM migrations ORDER BY id DESC LIMIT 1;
+    `);
+  }
 
-	async query(query: string): Promise<void> {
-		await this.db.query(sql`${query}`);
-	}
+  async query(query: string): Promise<void> {
+    await this.db.query(sql`${query}`);
+  }
 }
---
+++ src/core/migration/create.spec.ts
@@ -0,0 +1,24 @@
+import { readdir } from 'node:fs/promises';
+import { vol } from 'memfs';
+import { createMigration } from './create';
+
+jest.mock('node:fs/promises');
+
+describe('migrate', () => {
+  beforeEach(() => {
+    vol.reset();
+  });
+
+  it('should create a migration', async () => {
+    await createMigration('test');
+    const migrationDirectory = await readdir('migrations');
+    expect(migrationDirectory.length).toBe(1);
+
+    const migrationFiles = await readdir(`migrations/${migrationDirectory[0]}`);
+    expect(migrationFiles.length).toBe(4);
+    expect(migrationFiles.includes('up.sql')).toBe(true);
+    expect(migrationFiles.includes('down.sql')).toBe(true);
+    expect(migrationFiles.includes('before.sql')).toBe(true);
+    expect(migrationFiles.includes('after.sql')).toBe(true);
+  });
+});
