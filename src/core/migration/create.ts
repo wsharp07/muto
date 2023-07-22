a15ef5284fc09e9d1033441337753a15e4fe84dc
William Sharp <wsharp07@users.noreply.github.com>
2023-07-22 14:40:37 -0400

    Migrate from XO to ESlint  (#10)

--- src/core/migration/create.ts
+++ src/core/migration/create.ts
@@ -1,26 +1,38 @@
 import { writeFile, mkdir } from 'fs/promises';
 import { DEFAULT_CONFIG } from '@core/interface';
 
-const createDirectory = async (dir: string): Promise<void> => {
-  await mkdir(dir, { recursive: true });
+const createDirectory = async (directory: string): Promise<void> => {
+  await mkdir(directory, { recursive: true });
 };
 
 const createFile = async (
-  dir: string,
+  directory: string,
   name: string,
-  content: string,
+  content: string
 ): Promise<void> => {
-  await writeFile(`${dir}/${name}`, content);
+  await writeFile(`${directory}/${name}`, content);
 };
 
 export const createMigration = async (
   name: string,
-  config = DEFAULT_CONFIG,
+  config = DEFAULT_CONFIG
 ): Promise<void> => {
-  const dir = `${config.migrationDir}/${Date.now()}-${name}`;
-  await createDirectory(dir);
-  await createFile(dir, 'up.sql', '## Write your up migration here\n');
-  await createFile(dir, 'down.sql', '## Write your down migration here\n');
-  await createFile(dir, 'before.sql', '## Write your before migration here\n');
-  await createFile(dir, 'after.sql', '## Write your after migration here\n');
+  const directory = `${config.migrationDir}/${Date.now()}-${name}`;
+  await createDirectory(directory);
+  await createFile(directory, 'up.sql', '## Write your up migration here\n');
+  await createFile(
+    directory,
+    'down.sql',
+    '## Write your down migration here\n'
+  );
+  await createFile(
+    directory,
+    'before.sql',
+    '## Write your before migration here\n'
+  );
+  await createFile(
+    directory,
+    'after.sql',
+    '## Write your after migration here\n'
+  );
 };
