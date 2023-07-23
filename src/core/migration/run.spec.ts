56c5d89197456531342827c96ac9da0f2da51624
William Sharp <wsharp07@gmail.com>
2023-07-23 15:25:27 -0400

    adding tests for each adapter

--- src/core/migration/run.spec.ts
+++ src/core/migration/run.spec.ts
@@ -1,5 +1,6 @@
 import { vol } from 'memfs';
 import { PostgresDatabaseAdapter } from '@core/database/postgres';
+import { MySqlDatabaseAdapter } from '@core/database/mysql';
 import { createMigration } from './create';
 import { runMigrations } from './run';
 
@@ -10,35 +11,48 @@ describe('migrate', () => {
     vol.reset();
   });
 
-  it('should run migrations', async () => {
-    const databaseStub = new PostgresDatabaseAdapter(
-      'postgres://postgres:postgres@localhost:5432/test'
-    );
-    const createMigrationTableSpy = jest
-      .spyOn(databaseStub, 'createMigrationTable')
-      .mockResolvedValueOnce();
-    const getLatestMigrationSpy = jest
-      .spyOn(databaseStub, 'getLatestMigration')
-      .mockResolvedValueOnce('');
-    const executeMigrationUpSpy = jest
-      .spyOn(databaseStub, 'executeMigrationUp')
-      .mockResolvedValue();
-    const disposeSpy = jest.spyOn(databaseStub, 'dispose');
+  const testCases = [
+    {
+      adapterName: 'Postgres',
+      databaseStub: new PostgresDatabaseAdapter(
+        'postgres://postgres:postgres@localhost:5432/test'
+      ),
+    },
+    {
+      adapterName: 'MySQL',
+      databaseStub: new MySqlDatabaseAdapter('mysql://localhost:3306/mydb'),
+    },
+  ];
 
-    // Arrange
-    await createMigration('test1');
-    await createMigration('test2');
-    await createMigration('test3');
+  it.each(testCases)(
+    'should run migrations for $adapterName adapter',
+    async ({ databaseStub }) => {
+      const createMigrationTableSpy = jest
+        .spyOn(databaseStub, 'createMigrationTable')
+        .mockResolvedValueOnce();
+      const getLatestMigrationSpy = jest
+        .spyOn(databaseStub, 'getLatestMigration')
+        .mockResolvedValueOnce('');
+      const executeMigrationUpSpy = jest
+        .spyOn(databaseStub, 'executeMigrationUp')
+        .mockResolvedValue();
+      const disposeSpy = jest.spyOn(databaseStub, 'dispose');
 
-    // Act
-    await runMigrations(databaseStub, {
-      migrationDir: 'migrations',
-    });
+      // Arrange
+      await createMigration('test1');
+      await createMigration('test2');
+      await createMigration('test3');
 
-    // Assert
-    expect(createMigrationTableSpy).toBeCalledTimes(1);
-    expect(getLatestMigrationSpy).toHaveBeenCalledTimes(1);
-    expect(executeMigrationUpSpy).toHaveBeenCalledTimes(3);
-    expect(disposeSpy).toHaveBeenCalledTimes(1);
-  });
+      // Act
+      await runMigrations(databaseStub, {
+        migrationDir: 'migrations',
+      });
+
+      // Assert
+      expect(createMigrationTableSpy).toBeCalledTimes(1);
+      expect(getLatestMigrationSpy).toHaveBeenCalledTimes(1);
+      expect(executeMigrationUpSpy).toHaveBeenCalledTimes(3);
+      expect(disposeSpy).toHaveBeenCalledTimes(1);
+    }
+  );
 });
